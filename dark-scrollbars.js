a3a1dbd6b30fb74b5856c5a7989549ffaba4edaa
Kurian Thampy <kathampy@kathampy.com>
2020-07-06 21:45:20 -0700

    Create dark-scrollbars.js

---
+++ dark-scrollbars.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Dark Scrollbars
+// @namespace    https://kathampy.com/
+// @version      0.1
+// @description  Dark dynamic scrollbars on Firefox 72+
+// @author       Kurian Thampy
+// @match        *://*.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+// GM_addStyle("body { background-color: #000 !important; }");
+document.body.style.backgroundColor = "#000";
